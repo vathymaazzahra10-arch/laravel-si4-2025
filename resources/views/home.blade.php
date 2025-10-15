7ab5b5d8c558155dc99c0e9bcfe1823a5fc38dc4
vathymaazzahra10-arch <vathymaazzahra10@gmail.com>
2025-10-15 13:45:49 +0700

    Menggunakan bootstrap 5'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Profile</title>
+    <title>Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
@@ -41,9 +40,8 @@
     </nav>
 
     <div class="container" style="margin-left: 30px">
-        <h1>Ini Adalah Halaman Profile</h1>
+        <h1>Ini Adalah Halaman Home</h1>
     </div>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
         crossorigin="anonymous"></script>
